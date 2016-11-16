fd1982b5c427dfe4cdffb2d461adce19ad0616d4
isgi <isgiarriza@gmail.com>
2016-11-17 06:11:25 +0700

    first commit

---
+++ client/components/employee_list.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {createContainer} from 'meteor/react-meteor-data';
+import {Employees} from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
+
+const PER_PAGE = 2;
+
+const EmployeeList = (props) => {
+  // console.log(props.employees);
+  return (
+    <div>
+      <div className="employee-list">
+        {props.employees.map(employee =>
+          <EmployeeDetail key = {employee._id}  employee={employee}/>
+        )}
+      </div>
+      <button onClick={() => Meteor.subscribe('employees',40)}
+        className="btn btn-primary">
+        Load More...
+      </button>
+    </div>
+  );
+};
+
+export default createContainer(() => {
+  Meteor.subscribe('employees', PER_PAGE);
+  return {employees: Employees.find({}).fetch()}
+}, EmployeeList) ;
---
+++ imports/collections/employees.js
@@ -0,0 +1,5 @@
+//deklarasi collection
+
+import {Mongo} from 'meteor/mongo';
+
+export const Employees = new Mongo.Collection('employees');
